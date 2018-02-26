78299539a7375b831ee0e1ccb67c63927602bbcb
JC Ryan <jc_ryan@live.com>
2018-02-26 00:14:02 -0500

    testing filename for renaming after upload

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const multer = require('multer');
 var storage = multer.diskStorage({
     destination: function(req,file,callback) {
         callback(null, '../uploads')
-    },
+    }
+    ,
     filename: function(req,file,callback) {
         callback(null, file.originalname)
     }
 })
 
-const upload = multer({ storage: storage }); 
+//const upload = multer({ dest: '../uploads' }); 
+const upload = multer({ storage: storage })
 
 const app = express();
 
